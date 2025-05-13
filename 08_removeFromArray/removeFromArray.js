fa2cbc032cbeb18c6e239631aaa9d768dd925394
mariomartz0 <mariomartz2018@gmail.com>
2025-05-12 21:06:21 -0400

    Create function to remove elements from an array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, valueToRemove) {
+  // arr.length needs to be calculated every iteration because the size
+  // of the array could change
+  for (let i = 0; i < arr.length; i++) {
+    for (let argIndex = 1; argIndex <= arguments.length; argIndex++) {
+      if (arr[i] === arguments[argIndex]) {
+        arr.splice(i, 1);
+        // Decrements index to adjust for new array
+        if (i > 0) {
+          i--;
+        }
+      }
+    }
+  }
+
+  return arr;
 };
 
 // Do not edit below this line
