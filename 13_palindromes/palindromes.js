323d7cd1cf2af7ed73e8b6b128853d734ccc2315
mariomartz0 <mariomartz2018@gmail.com>
2025-05-29 22:49:45 -0400

    Complete palindrome function

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // converts string to lower case, removes punctuation, removes white space
+  string = string.toLowerCase().replaceAll(/\p{P}/gu, "").replaceAll(" ", "");
+  // reverses the string
+  let stringReversed = Array.from(string).reverse().join("");
 
+  return string === stringReversed;
 };
 
 // Do not edit below this line
