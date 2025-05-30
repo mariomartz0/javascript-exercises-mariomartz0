a9b4d0c338e3f7a420d7f44a5c524f61f346c57a
mariomartz0 <mariomartz2018@gmail.com>
2025-05-30 16:18:53 -0400

    Complete findTheOldest function

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldestPerson, currentPerson) => {
+    const oldestPersonAge =
+      getYearOfDeath(oldestPerson) - oldestPerson.yearOfBirth;
+    const currentPersonAge =
+      getYearOfDeath(currentPerson) - currentPerson.yearOfBirth;
 
+    if (oldestPersonAge > currentPersonAge) {
+      return oldestPerson;
+    }
+
+    return currentPerson;
+  }, people[0]);
 };
 
+function getYearOfDeath(person) {
+  let yearOfDeath = new Date().getFullYear();
+
+  if (Object.hasOwn(person, "yearOfDeath")) {
+    yearOfDeath = person.yearOfDeath;
+  }
+
+  return yearOfDeath;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
